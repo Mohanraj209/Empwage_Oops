6219b863496e200f0d5dbd9e061661b9a8803146
Mohanraj209 <mohanraj1715@gmail.com>
2022-02-26 23:59:46 +0530

    Displaying Message

---
+++ Empwage_Oops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empwage_Oops
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("--------**** Welcome to Employee Wage Computation ****--------");
+        }
+    }
+}
